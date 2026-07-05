164b102b849e98f7d51ebf067f7376e8825342fa
Igor <djiojoigor@yahoo.com>
2026-07-05 18:14:57 +0200

    fix: terraform fmt

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,7 +1,7 @@
 # Configure le backend remote (rempli APRÈS le bootstrap).
 terraform {
   backend "s3" {
-    bucket         = "eip-tfstate-CHANGE-ME-123"   # <-- reporte ici le bucket du bootstrap
+    bucket         = "eip-tfstate-CHANGE-ME-123" # <-- reporte ici le bucket du bootstrap
     key            = "dev/terraform.tfstate"
     region         = "eu-west-3"
     dynamodb_table = "eip-tflock"
